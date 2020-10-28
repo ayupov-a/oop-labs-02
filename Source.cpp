e2986a56d49adf021835fd61cc5247a4bdf432dd
Aigiz Ayupov <aigiz.ayupov228@gmail.com>
2020-10-28 16:48:27 +0500

    Creating classes, atributes, constructors, destructors

--- Source.cpp
+++ Source.cpp
@@ -2,10 +2,43 @@
 
 using namespace std;
 
+class Quadrangle {
+protected:
+	double area;
+	double insc_rad;
+	double desc_rad;
+public:
+	Quadrangle() {
+		cout << "Quadrangle()" << "\n\n";
+		this->area = 1;
+		this->insc_rad = 5;
+		this->desc_rad = 6;
+	}
+	Quadrangle(double area, double insc_rad, double desc_rad) {
+		cout << "Quadrangle(double area, double insc_rad, double desc_rad)" << "\n\n";
+		this->area = area;
+		this->insc_rad = insc_rad;
+		this->desc_rad = desc_rad;	
+	}
+	Quadrangle(const Quadrangle &q) {
+		cout << "Quadrangle(const Quadrangle &q)" << "\n\n";
+		area = q.area;
+		insc_rad = q.insc_rad;
+		desc_rad = q.desc_rad;
+	}
+	~Quadrangle() {
+		cout << "~Quadrangle()" << endl;
+		printf("%0.1f %0.1f %0.1f\n", area, insc_rad, desc_rad);
+	}
+};
+
+
 int main() {
-	int a;
-	int b;
-	cin >> a >> b;
-	cout << "a + b = " << a + b << endl;
+	{
+		Quadrangle q;
+		Quadrangle q1(5.1, 6, 7);
+		Quadrangle q2(q1);
+	}
+
 	return 0;
 }
