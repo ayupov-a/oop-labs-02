d515eb1246eb10d80473467e7d97e85bde711cf2
Aigiz Ayupov <aigiz.ayupov228@gmail.com>
2020-11-10 13:19:07 +0500

    Class renaming

--- Source.cpp
+++ Source.cpp
@@ -3,57 +3,55 @@
 
 using namespace std;
 
-class Quadrangle {
+class Polygon {
 protected:
 	double area;
-	double insc_rad;
-	double desc_rad;
 public:
 	
-	Quadrangle() {
-		cout << "Quadrangle()" << "\n\n";
+	Polygon() {
+		cout << "Polygon()" << "\n\n";
 		this->area = 1;
-		this->insc_rad = 5;
-		this->desc_rad = 6;
 	}
-	Quadrangle(double area, double insc_rad, double desc_rad) {
-		cout << "Quadrangle(double area, double insc_rad, double desc_rad)" << "\n\n";
+	Polygon(double area) {
+		cout << "Polygon(double area)" << "\n\n";
 		this->area = area;
-		this->insc_rad = insc_rad;
-		this->desc_rad = desc_rad;	
 	}
-	Quadrangle(const Quadrangle &q) {
-		cout << "Quadrangle(const Quadrangle &q)" << "\n\n";
+	Polygon(const Polygon &q) {
+		cout << "Polygon(const Polygon &q)" << "\n\n";
 		area = q.area;
-		insc_rad = q.insc_rad;
-		desc_rad = q.desc_rad;
 	}
-	~Quadrangle() {
-		cout << "~Quadrangle()" << endl;
-		printf("%0.1f %0.1f %0.1f\n", area, insc_rad, desc_rad);
+	~Polygon() {
+		cout << "~Polygon()" << endl;
+		printf("%0.1f \n", area);
 	}
 	void area_increase(double areainc) {
 		area = area + areainc;
-		insc_rad = sqrt(area * 2);
-		desc_rad = sqrt(area * 4);
 	}
 	void reset();
 };
 
-void Quadrangle::reset() {
+void Polygon::reset() {
 	area = 1;
-	insc_rad = 1;
-	desc_rad = 1;
+
 }
+
+class Rectangle : public Polygon {
+private:
+	float side_a, side_b;
+public:
+	Rectangle() {
+
+	}
+};
 
 
 int main() {
 	/*{
-		Quadrangle q;
-		Quadrangle q1(5.1, 6, 7);
-		Quadrangle q2(q1);
+		Polygon q;
+		Polygon q1(5.1, 6, 7);
+		Polygon q2(q1);
 	}*/
-	Quadrangle *q = new Quadrangle(5.1, 6, 7);
+	Polygon *q = new Polygon(5.1);
 	q->reset();
 	q->area_increase(2);
 
